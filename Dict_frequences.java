bc4cf0912c76393cb9f562fdcd3641df0baccc30
Alemanu211 <90306622+Alemanu211@users.noreply.github.com>
2022-05-12 23:17:18 +0200

    Add files via upload

--- Dict_frequences.java
+++ Dict_frequences.java
@@ -1,16 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Dict_frequences {
 	public static Object get_data () {
+		String file = "C:\\Users\\Alexandre\\Desktop\\Decompression Huffman/exemple_freq.txt";
 		Map <String, Integer> freq = new HashMap<>();
-		freq.put("b", 1);
-		freq.put("j", 1);
-		freq.put("n", 1);
-		freq.put("r", 1);
-		freq.put("u", 1);
-		freq.put("!", 2);
-		freq.put("o", 2);
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"UTF-8"));
+			String line = reader.readLine();
+			
+			while (line != null) {
+				line = reader.readLine();
+				if (line != null) {
+					freq.put(line.substring(0,1), Integer.parseInt(line.substring(2,3)));
+				}
+			}
+			reader.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
 		return freq;
 	}
 	
